667138a13720c067e661b3fd969aaf430aef5a85
Bri Dotson <bridotson@gmail.com>
2014-10-08 15:54:09 -0700

    add login capability using session

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,22 @@
 class HomeController < ApplicationController
 
   def login
-    raise.params.inspect
-
-
+    # raise params.inspect
     # find vendor in the database
       # itterate over vendor datbase with find by parms of the login
     # session
-
-    Vendor.find_by(name: params[:login][:vendor_name])
-
+    if Vendor.find_by(name: params[:login][:vendor_name])
+      session[:user_id] = Vendor.find_by(name: params[:login][:vendor_name]).id
+      @current_vendor = Vendor.find(session[:user_id])
+      render :index
+    else
+      render :index
+    end
 
   end
 
 
   def index
-
 
   end
 
